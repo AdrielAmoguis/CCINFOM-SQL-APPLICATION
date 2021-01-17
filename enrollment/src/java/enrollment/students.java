2b272d02fdb90d3d144208c74f184f208ea8a72c
CCS Learner <tiffany_anne_so@dlsu.edu.ph>
2021-01-17 13:28:14 +0800

    Updated students

--- enrollment/src/java/enrollment/students.java
+++ enrollment/src/java/enrollment/students.java
@@ -3,13 +3,104 @@ import java.sql.*;
 
 public class students {
     
+    public int studentid;
+    public String completename;
+    public String degreeid;
+    
     public students () {};
-    public int modRecord()  { return 0; };
-    public int delRecord()  { return 0; };
-    public int addRecord()  { return 0; };
-    public int viewRecord() { return 0; };
+    
+    public int modRecord()  { 
+    try{
+            Connection conn;     
+            // 2. Connect to your DB
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3310/enrolldb?useTimezone=true&serverTimezone=UTC&user=root&password=12345678");
+            System.out.println("Connection Successful");
+            // 4. Prepare our INSERT Statement
+            PreparedStatement pstmt = conn.prepareStatement("UPDATE students          " +
+                                                            "SET    degreeid   = ? " + 
+                                                            "WHERE  studentid = ? ");
+            // 5. Supply the statement with values
+            pstmt.setString (1, degreeid );
+            pstmt.setInt    (2, studentid);
+            pstmt.executeUpdate();   
+            pstmt.close();
+            conn.close();
+            return 1;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());  
+            return 0;
+        }         
+    }
+    
+    public int delRecord()  { 
+        try {
+            Connection conn;     
+            // 2. Connect to your DB
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3310/enrolldb?useTimezone=true&serverTimezone=UTC&user=root&password=12345678");
+            // 3. Indicate a notice of successful connection
+            System.out.println("Connection Successful");
+            // 4. Prepare our INSERT Statement
+            PreparedStatement pstmt = conn.prepareStatement("DELETE FROM students WHERE studentid=?");
+            // 5. Supply the statement with values
+            pstmt.setInt    (1, studentid );
+            // 6. Execute the SQL Statement
+            pstmt.executeUpdate();   
+            pstmt.close();
+            conn.close();
+            return 1;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());  
+            return 0;
+        }       }
+    
+    public int addRecord()  { 
+        try {
+            Connection conn;     
+            // 2. Connect to your DB
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3310/enrolldb?useTimezone=true&serverTimezone=UTC&user=root&password=12345678");
+            System.out.println("Connection Successful");
+            PreparedStatement pstmt = conn.prepareStatement("INSERT INTO students VALUES (?,?,?)");
+            pstmt.setInt    (1, studentid );
+            pstmt.setString (2, completename);
+            pstmt.setString (3, degreeid);
+            pstmt.executeUpdate();   
+            pstmt.close();
+            conn.close();
+            return 1;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());  
+            return 0;
+        }       
+    }
+    public int viewRecord() { 
+    try {
+            Connection conn;     
+            // 2. Connect to your DB
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3310/enrolldb?useTimezone=true&serverTimezone=UTC&user=root&password=12345678");
+            System.out.println("Connection Successful");
+            // 4. Prepare our INSERT Statement
+            PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM students WHERE studentid=?");
+            pstmt.setInt    (1, studentid );
+            // 6. Execute the SQL Statement
+            ResultSet rs = pstmt.executeQuery();   
+            
+            // 7. Get the results
+            while (rs.next()) {
+                studentid  = rs.getInt("studentid");
+                completename = rs.getString ("completename");
+                degreeid    = rs.getString("degreeid");
+            }
+            rs.close();
+            pstmt.close();
+            conn.close();
+            return 1;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());  
+            return 0;
+        }           }
     
     public static void main(String args[]) {
        
     }
 }
+
