2dda13ce2caf6bcba3a020ee228c80a2ab82b07d
Adriel Amoguis <adriel_amoguis@dlsu.edu.ph>
2021-01-17 17:02:19 +0800

    Added CourseDegree delRecord()
    Need to review how the coursedegree class should be treated in the overall DDL. It currently serves as nothing but as the connection between the course table and the degree table, which is then queryable from the enrollment module. I will be developing this initially using a static method to get courses. Should the group decide on another design/implementation for this, I'll modify it accordingly.

---
+++ enrollment/src/java/enrollment/DBInfo.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package enrollment;
+
+/**
+ * This class simply provides the jdbcConnectionString.
+ * @author AMOGUIS, Adriel Isaiah V.
+ */
+public class DBInfo {
+    public static String jdbcConnectionString = "jdbc:mysql://localhost:3306/enrolldb?useTimezone=true&serverTimezone=UTC&user=root&password=12345678";
+}
--- enrollment/src/java/enrollment/coursedegree.java
+++ enrollment/src/java/enrollment/coursedegree.java
@@ -1,15 +1,127 @@
 package enrollment;
 import java.sql.*;
+import java.util.ArrayList;
 
+/**
+ * This Data Access Object execute operations on the database for the
+ * coursedegree table.
+ * @author AMOGUIS, Adriel Isaiah V.
+ */
 public class coursedegree {
     
-    public coursedegree () {};
-    public int modRecord()  { return 0; };
-    public int delRecord()  { return 0; };
-    public int addRecord()  { return 0; };
-    public int viewRecord() { return 0; };
+    // Class Attributes
+    public String courseid;
+    public String degree;
+    
+    // USING DEFAULT CONSTRUCTOR
+    public coursedegree() 
+    {
+        this.courseid = null;
+        this.degree = null;
+    }
+    
+    public int modRecord()
+    {
+        return 0;
+    }
+    
+    public int delRecord()
+    {
+        // Invoke the connection
+        Connection dbCon;
+        try
+        {
+            dbCon = DriverManager.getConnection(DBInfo.jdbcConnectionString);
+            System.out.println("[CourseDegree DAO - DELETE] Database Connection Established.");
+            
+            // Prepare SQL statement
+            PreparedStatement sqlStatement = dbCon.prepareStatement("DELETE FROM coursedegree WHERE courseid = ? AND degree = ?");
+            sqlStatement.setString(1, this.courseid);
+            sqlStatement.setString(2, this.degree);
+            
+            // Execute statement and get results
+            sqlStatement.executeUpdate();
+            
+            // Close the connection
+            sqlStatement.close();
+            dbCon.close();
+        }
+        catch (SQLException ex)
+        {
+            System.out.printf("[CourseDegree DAO - DELETE] Exception Occurred Executing SQL:\n%s", ex.getMessage());
+            return 1;
+        }
+        
+        return 0;
+    }
+    
+    public int addRecord()
+    {
+        return 0;
+    }
+    
+    public int viewRecord()
+    {
+//        // Invoke the connection
+//        Connection dbCon;
+//        try
+//        {
+//            dbCon = DriverManager.getConnection(DBInfo.jdbcConnectionString);
+//            System.out.println("[CourseDegree DAO - VIEW] Database Connection Established.");
+//            
+//            // Query using degreeid or degree?
+//            String qryCond;
+//            if(this.courseid != null && this.degree == null)
+//                qryCond = this.courseid;
+//            else if(this.courseid == null && this.degree != null)
+//                qryCond = this.degree;
+//            else
+//            {
+//                System.out.println("[CourseDegree DAO - VIEW] Ambiguous View Request:\nOne of the field attributes must be null.\nExample: " +
+//                        "if courseid == null, then this DAO will query for the courseid ");
+//                return 1;
+//            }
+//            
+//            // Prepare SQL statement
+//            PreparedStatement sqlStatement = dbCon.prepareStatement("SELECT courseid, degree FROM coursedegree WHERE courseid = ?");
+//            sqlStatement.setString(1, this.courseid);
+//            
+//            // Execute statement and get results
+//            ResultSet results = sqlStatement.executeQuery();
+//            
+//            // Parse Results
+//            while(results.next())
+//            {
+//                this.degree.add(results.getString("degree"));
+//            }
+//            
+//            // Close the connection
+//            results.close();
+//            sqlStatement.close();
+//            dbCon.close();
+//        }
+//        catch (SQLException ex)
+//        {
+//            System.out.printf("[CourseDegree DAO - VIEW] Exception Occurred Executing SQL:\n%s", ex.getMessage());
+//            return 1;
+//        }
+        
+        return 0;
+    }
     
     public static void main(String args[]) {
-       
+//        // Test Script for coursedegree
+//        // Instantiate class
+//        coursedegree cd = new coursedegree();
+//        
+//        // VIEW RECORDS
+//        System.out.println("VIEW RECORDS");
+//        cd.courseid = "CCPROG1";
+//        cd.viewRecord();
+//        System.out.println(cd.toString());
+//        
+//        // MODIFY RECORDS
+//        cd = new coursedegree();
+//        
     }
 }
