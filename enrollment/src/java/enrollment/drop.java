32c6596d89615a1025429866d9bca1bf1b8ec7e7
Adriel Amoguis <adriel_amoguis@dlsu.edu.ph>
2021-01-27 01:28:26 +0800

    Ready for merge

--- enrollment/src/java/enrollment/drop.java
+++ enrollment/src/java/enrollment/drop.java
@@ -13,7 +13,23 @@ public class drop {
     public ArrayList<enrollment>    EnrollmentList  = new ArrayList<> ();
     public ArrayList<enrollment>    DropList        = new ArrayList<> ();
 
-    public drop() {};                                   // perform all the necessary data loading from DB
+    public drop() // perform all the necessary data loading from DB
+    {    }
+    
+    public int loginStudent(int studentid, int currTerm, int syear)
+    {
+        // Initialize the student object
+        this.Student.studentid = studentid;
+        this.Student.viewRecord();
+        
+        // Load the student's enrollment list
+        
+        
+        
+        
+        return 1;
+    }
+    
     public int clearDrop ()         {   return 0;   }   // clears dropping data of the student
     public int loadEnrollment ()    {   return 0;   }   // load enrollment data of the student 
     public int confirmDrop()        {   return 0;   }   // saves dropping data into the Database
