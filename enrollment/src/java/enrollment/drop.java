d977dd02632a7d561936906af701d97898ceeeb6
Dev-Acceus <ventilantum@gmail.com>
2021-01-27 14:26:44 +0800

    Finished enroll and drop TAOs

--- enrollment/src/java/enrollment/drop.java
+++ enrollment/src/java/enrollment/drop.java
@@ -9,29 +9,112 @@ import java.util.*;
 
 public class drop {
   
-    students                        Student         = new students();
+    public students                 Student         = new students();
+    public int                      cur_term;
+    public int                      sYear;
     public ArrayList<enrollment>    EnrollmentList  = new ArrayList<> ();
     public ArrayList<enrollment>    DropList        = new ArrayList<> ();
-
-    public drop() // perform all the necessary data loading from DB
-    {    }
+    public boolean                  loggedin;
     
-    public int loginStudent(int studentid, int currTerm, int syear)
+    // Note: Loading will be done after student logs in
+    public drop() // perform all the necessary data loading from DB
+    {    
+        loggedin = false;
+    }
+    
+    
+    public int loginStudent(int studentid, int cur_term, int sYear)
     {
         // Initialize the student object
         this.Student.studentid = studentid;
         this.Student.viewRecord();
         
+        this.cur_term = cur_term;
+        this.sYear = sYear;
+        this.loggedin = true;
+        
         // Load the student's enrollment list
         
-        
-        
-        
+   
         return 1;
     }
     
-    public int clearDrop ()         {   return 0;   }   // clears dropping data of the student
-    public int loadEnrollment ()    {   return 0;   }   // load enrollment data of the student 
-    public int confirmDrop()        {   return 0;   }   // saves dropping data into the Database
+    // clears dropping data of the student
+    public int clearDrop ()         {   
+        this.DropList.clear();
+        return 0;   
+    }   
+    
+    // load enrollment data of the student 
+    public int loadEnrollment ()    {   
+        // Load student's currently enrolled courses based on term & schoolyear
+        Connection dbCon;
+        try
+        {
+            dbCon = DriverManager.getConnection(DBInfo.jdbcConnectionString);
+            System.out.println("[Drop TAO - LOAD] Database Connection Established.");
+            
+            // Obtain all enrollment records with studentid, term, schoolyear         
+            PreparedStatement sqlStatement = dbCon.prepareCall("SELECT studentid, courseid, term, schoolyear " +
+                                                               "FROM enrollment " +
+                                                               "WHERE studentid = ?, " +
+                                                               "AND   term = ?, " +
+                                                               "AND   schoolyear = ?");
+                                                                                 
+            sqlStatement.setInt(1, this.Student.studentid);
+            sqlStatement.setInt(2, this.cur_term);
+            sqlStatement.setInt(3, this.sYear);
+            
+            ResultSet rs = sqlStatement.executeQuery();
+            // Clear list before appending
+            EnrollmentList.clear();
+            // Read the results
+            while(rs.next())
+            {
+                enrollment new_en = new enrollment();
+                new_en.studentid = rs.getInt("studentid");
+                new_en.courseid = rs.getString("courseid");
+                new_en.term = rs.getInt("term");
+                new_en.schoolyear = rs.getInt("schoolyear");
+               
+                EnrollmentList.add(new_en);
+            }
+            
+            // Close the connections
+            rs.close();
+            sqlStatement.close();
+            dbCon.close();
+        }
+        catch (SQLException ex)
+        {
+            System.out.printf("[Drop TAO - LOAD] Exception Occurred Executing SQL:\n%s", ex.getMessage());
+            return 0;
+        }
+        return 1;   
+    }   
+    
+    
+    // Adds a course to enrollment list
+    public int addDropCourse(String courseid){
+        enrollment new_en = new enrollment();
+        // initialize attributes before adding to list
+        new_en.studentid = this.Student.studentid;
+        new_en.courseid = courseid;
+        new_en.term = this.cur_term;
+        new_en.schoolyear = this.sYear;
+        
+        // append to enrollment list
+        DropList.add(new_en);
+        return 1;
+    }
+    
+    
+    // saves dropping data into the Database
+    public int confirmDrop()        {   
+        for(int i = 0; i< DropList.size(); i++)
+            DropList.get(i).delRecord();
+        
+        return 0;   
+    }   
     
 }
--- enrollment/src/java/enrollment/enroll.java
+++ enrollment/src/java/enrollment/enroll.java
@@ -5,11 +5,11 @@ import java.util.*;
 public class enroll {
 
     public students                 Student         = new students();
-    public int                      cur_term        = 0;
-    public int                      sYear           = 0;
+    public int                      cur_term;
+    public int                      sYear;
     public ArrayList<enrollment>    EnrollmentList  = new ArrayList<> ();
     public ArrayList<coursedegree>  CourseList      = new ArrayList<> ();
-    public boolean loggedin;
+    public boolean                  loggedin;
  
     // Note: Loading will be done after student logs in
     public enroll() // perform all the necessary data loading from DB
@@ -17,18 +17,19 @@ public class enroll {
         loggedin = false;
     }                                 
     
+    // clears enrollment data of the student 
     public int clearEnrollment ()   
     {   
         this.EnrollmentList.clear();
         return 0;   
-    }   // clears enrollment data of the student 
+    }   
     
     
     // Load valid courses into the course list
+    // Note: student must be logged in first
     public int loadCourses ()        
     {   
         // Load valid courses based on student's degree
-        String studentdegree = Student.degreeid;
         Connection dbCon;
         try
         {
@@ -45,8 +46,12 @@ public class enroll {
                                                                                  "WHERE studentid = ? " + 
                                                                                  "AND term = ? " + 
                                                                                  "AND schoolyear = ? )");
-                                                                                 
-            sqlStatement.setString(1, studentdegree);
+            // Main query                                                                     
+            sqlStatement.setString(1, Student.degreeid);
+            // Subquery
+            sqlStatement.setInt(2, Student.studentid);
+            sqlStatement.setInt(3, this.cur_term);
+            sqlStatement.setInt(4, this.sYear);
             
             ResultSet rs = sqlStatement.executeQuery();
             // Clear list before appending
@@ -74,18 +79,45 @@ public class enroll {
         return 1;  
     }   
     
-    public void loginStudent(int studentid, int cur_term, int sYear){
+    public int loginStudent(int studentid, int cur_term, int sYear){
         Student.studentid = studentid;
         // Get record of studentid from database
         Student.viewRecord();
         this.cur_term = cur_term;
         this.sYear = sYear;
         this.loggedin = true;
+        
+        // Load the student's valid courses
+        loadCourses();
+        
+        return 1;
     }
+    
+    // Adds a course to enrollment list
+    public int addCourse(String courseid){
+        enrollment new_en = new enrollment();
+        // initialize attributes before adding to list
+        new_en.studentid = this.Student.studentid;
+        new_en.courseid = courseid;
+        new_en.term = this.cur_term;
+        new_en.schoolyear = this.sYear;
+        
+        // append to enrollment list
+        EnrollmentList.add(new_en);
+        return 1;
+    }
+    
+    // Saves enrollment data into the Database
     public int confirmEnrollment()  
-    {   return 0;   }   // saves enrollment data into the Database
+    {   
+        // Add all enrollment records to DB
+        for (int i = 0; i<EnrollmentList.size(); i++)
+            EnrollmentList.get(i).addRecord();
+        
+        return 0;   
+    }   
     
-    
+    // Helper method for JSP to not display coursedegree that is already in enrollmentlist
     public boolean isEnrolled(String courseid)
     {
         enrollment em = new enrollment();
